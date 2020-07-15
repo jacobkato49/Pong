1d24825eca986480b363b4df472870751cec6310
Jacob Kato <jacobkato49@yahoo.com>
2020-07-14 21:21:05 -0700

    updated movement for right paddle

--- app.py
+++ app.py
@@ -47,9 +47,28 @@ def paddle_a_up():
     y += 20 #adds 20 pixels to y
     paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20 #adds 20 pixels to y
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20 #adds 20 pixels to y
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20 #adds 20 pixels to y
+    paddle_b.sety(y)
+
+
 #keyboard binding
 wn.listen() #listen for keyboard input
 wn.onkeypress(paddle_a_up, 'w')
+wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
 
 #main game loop
 while True:
